54d982d334128b6d148c5fbb88ea7ca1f8ff1411
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-07-28 12:17:33 +0530

    registerpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import HomePage from "./pages/HomePage";
+import SignUp from "./pages/SignUp";
+
 
 function App() {
   return (
     <div>
       <HomePage />
+      {/* <SignUp/> */}
     </div>
   );
 }
